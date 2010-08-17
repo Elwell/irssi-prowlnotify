3817d197fba264857a24dd51aee45f01df20d53a
Simon Manning <simon@quaqsim.net>
2010-08-17 22:12:10 +0100

    Removed auto_away stuff and explicitly looking for MSGLEVEL_HILIGHT events instead of nick

--- prowlnotify.pl
+++ prowlnotify.pl
@@ -5,6 +5,8 @@ use warnings;
 #
 # Version history
 #
+# 0.3
+#       Removed auto_away stuff and changed to check for hilight
 # 0.2
 #	Modify to use new API keys, much better!
 # 0.1
@@ -18,22 +20,18 @@ use vars qw($VERSION %IRSSI %config);
 
 use LWP::UserAgent;
 
-$VERSION = "0.2";
+$VERSION = "0.3";
 
 %IRSSI = (
 	authors => "Denis Lemire",
 	contact => "denis\@lemire.name",
 	name => "prowl",
-	description => "Sets nick away when client discconects from the "
-		. "irssi-proxy sends messages to an iPhone via prowl.",
+	description => "Sends messages to an iPhone via prowl.",
 	license => "GPLv2",
 	url => "http://www.denis.lemire.name",
 );
 
-$config{away_level} = 0;
-$config{awayreason} = 'Auto-away because client has disconnected from proxy.';
 $config{debug} = 0;
-$config{clientcount} = 0;
 
 sub debug
 {
@@ -99,63 +97,23 @@ sub send_prowl
 	}
 }
 
-sub client_connect
-{
-	my (@servers) = Irssi::servers;
- 
-	$config{clientcount}++;
-	debug("Client connected.");
-
-	# setback
-	foreach my $server (@servers) {
-		# if you're away on that server send yourself back
-		if ($server->{usermode_away} == 1) {
-			$server->send_raw('AWAY :');
-		}
-	}
-}
-
-sub client_disconnect
-{
-	my (@servers) = Irssi::servers;
-	debug('Client Disconnectted');
-
-	$config{clientcount}-- unless $config{clientcount} == 0;
-
-	# setaway
-	if ($config{clientcount} <= $config{away_level}) {
-		# ok.. we have the away_level of clients connected or less.
-		foreach my $server (@servers) {
-			if ($server->{usermode_away} == "0") {
-				# we are not away on this server allready.. set the autoaway
-				# reason
-				$server->send_raw(
-					'AWAY :' . $config{awayreason}
-				);
-			}
-		}
-	}
-}
-
 sub msg_pub
 {
-	my ($server, $data, $nick, $mask, $target) = @_;
-	 
-	if ($server->{usermode_away} == "1" && $data =~ /$server->{nick}/i) {
-		debug("Got pub msg with my name");
-		send_prowl ("Mention", $nick . ': ' . $data);
+	my ($dest, $text, $stripped) = @_;
+	
+	if ($dest->{level} & MSGLEVEL_HILIGHT) {
+		send_prowl ("Mention", $stripped);
 	}
 }
 
 sub msg_pri
 {
 	my ($server, $data, $nick, $address) = @_;
-	if ($server->{usermode_away} == "1") {
+
+	if (Irssi::active_win()->get_active_name() ne $nick) {
 		send_prowl ("Private msg", $nick . ': ' . $data);
 	}
 }
 
-Irssi::signal_add_last('proxy client connected', 'client_connect');
-Irssi::signal_add_last('proxy client disconnected', 'client_disconnect');
-Irssi::signal_add_last('message public', 'msg_pub');
+Irssi::signal_add_last('print text', 'msg_pub');
 Irssi::signal_add_last('message private', 'msg_pri');
